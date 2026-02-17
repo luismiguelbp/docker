eb882e8257a2b370103bc7175c35f08587fd1f3d
Luis Miguel BP <luismiguelbp@gmail.com>
2026-02-17 11:47:57 +0100

    fix: Update Docker GPG key URL in docker-setup script for dynamic OS support

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -162,11 +162,11 @@ install_docker_debian() {
   $SUDO apt update
   $SUDO apt install ca-certificates curl
   $SUDO install -m 0755 -d /etc/apt/keyrings
-  $SUDO curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+  $SUDO curl -fsSL "https://download.docker.com/linux/${ID}/gpg" -o /etc/apt/keyrings/docker.asc
   $SUDO chmod a+r /etc/apt/keyrings/docker.asc
 
   echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/${ID} \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     $SUDO tee /etc/apt/sources.list.d/docker.list > /dev/null
   $SUDO apt update
